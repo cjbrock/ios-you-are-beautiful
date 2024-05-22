ec1fd0c7d5625fcda6afac69eb6588d43b53298d
Corinna Brock <corinna.brock@gmail.com>
2024-05-22 11:33:35 -0400

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Corinna Moore on 5/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
